fa6d05fedd38dfe610c32ef168ac38e58b2214b9
hiroko.hiraoka <chibichibikichi@gmail.com>
2020-02-10 15:30:11 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <h2>おみくじの結果：無し</h2>
+        <form action="sample.php"method="POST">
+            <button type="submit" name="act" value="drow">おみくじをひく！</button>
+        </form>
+    </body>
+</html>
